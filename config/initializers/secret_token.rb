577c451c206c4230b1a1aa0d5af631946c1863e2
Juan Mercedes <j.mercedes@thedesignpill.com>
2013-10-22 18:33:16 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mersa::Application.config.secret_token = 'dd3fcc47544d0327e3c6f72102300fa9e4053e378fd7b124dd1f7607a1e26aa155a4846eb9e450716456f4772bfeb1d53defa6d31b1d715499456b79dfaeccc3'
